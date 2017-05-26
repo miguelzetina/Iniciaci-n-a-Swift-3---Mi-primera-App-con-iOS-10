a5e37ca4ebc17439835f3f69bb544550a304da65
Miguel Zetina <Loefthur26@gmail.com>
2017-05-25 23:15:02 -0500

    Sección 2: Variables y constantes

---
+++ varsAndCons.playground/Contents.swift
@@ -0,0 +1,66 @@
+var str : String = "Hola, playground"
+
+str = "Hola, mundo"
+
+//Ejercicio de variables
+//Sección 2, clase 10
+//Crea una variable llamada age, y configúrala para que muestre tu edad.
+
+var age : Int = 21
+
+age
+
+age = 24
+
+age
+
+age = 21
+
+var name : String = "Miguel Zetina"
+
+str = "Hola \(name), tengo \(age) años"
+
+var nickname : String = "Mike"
+
+
+//Declarar constantes con "let"
+let birthDate : Int = 1995
+
+let pi : Float = 3.14159625
+
+str
+
+pi
+
+var hola = "Hola"
+
+age
+
+hola
+
+var holaQueHace = "¿Hola qué hace?"
+
+//Ejercicio de modificación de variables
+//Sección 2, clase 12
+//Crea una variable llamada weather, y configúrala para que muestre "Soleado". 
+//En otra línea de código cambia el valor de weather  y haz que tome el valor "Lluvioso".
+
+var weather : String = "Soleado"
+
+weather = "Lluvioso"
+
+// Ejercicio de constantes
+// Sección 2, clase 14
+// Crea una constante llamada birthYear y haz que tome el valor de tu fecha de nacimiento.
+
+let birthYear : Int = 1995
+
+
+
+
+
+
+
+
+
+
