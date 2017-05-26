3c909cc0f44a7ef2fd331260ed8efecce7d1ea31
Miguel Zetina <Loefthur26@gmail.com>
2017-05-26 00:40:54 -0500

    Sección 3: Strings

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,34 @@
+
+var name : String = "Miguel Zetina" //Declaración explicita
+
+name.uppercased()
+
+name.lowercased()
+
+var id = "" // Declaración implícita vacía
+
+var idUser : String //Declaración explicita vacía
+
+idUser = "1321313"
+
+
+//Ejercicios con Strings
+//Sección 3, clase 19
+//1.- Crea una variable de tipo string llamada favouriteFood e inicialízala a tu comida favorita.
+//2.- Crea una variable de tipo string llamada petName sin inicializar.
+//3.- ¿Tienes mascota? Inicializa la variable petName anterior en otra línea de código.
+
+var favouriteFood : String = "Mole de olla"
+var petName : String
+petName = "Ángel"
+
+var laptop :String = "💻"
+
+var 🖥 = "iMAC"
+
+//Ejercicios con Emojis
+//Sección 3, clase 21
+//Crea una variable de tipo string y nómbrala con un emoji. Asígnale cualquier valor
+
+var 💻 : String = "💻💻💻💻"
+
