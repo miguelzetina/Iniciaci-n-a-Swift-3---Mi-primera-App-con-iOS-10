5353f233237b3370e7735819033e4db5eacabd42
Miguel Zetina <Loefthur26@gmail.com>
2017-05-26 00:48:33 -0500

    Sección 3: Ints

---
+++ Ints.playground/Contents.swift
@@ -0,0 +1,20 @@
+var age : Int = 28
+
+// var age : Int
+
+age = 21
+
+age + 4
+age - 4
+age * 4
+age / 4
+age % 4
+age += 1
+age -= 1
+age
+
+//Ejercicios de números enteros
+//Sección 3, clase 23
+//Crea una variable entera llamada wallet y asígnale el valor de la cantidad de dinero que lleves ahora mismo en la cartera (déjate de contar centimillos, solo euros o dólares enteros)
+
+var wallet : Int = 23
