2a7beff912351f873e9e48601b08672bd599f5a1
Miguel Zetina <Loefthur26@gmail.com>
2017-05-26 00:54:41 -0500

    Sección 3: Ejercicios con nteros y strings

--- StringsInter.playground/Contents.swift
+++ StringsInter.playground/Contents.swift
@@ -2,3 +2,18 @@ let name = "Miguel Zetina"
 let age = 21
 
 let presentation = "Hola, me llamo \(name) y tengo \(age) años"
+
+
+//Ejercicios de enteros y Strings
+//Sección 3, clase 25
+//Crea una variable entera con el valor 2.
+//Crea otra variable entera con el valor 4.
+//Crea un string que ponga "2+2 = 4"
+//Sustituye en el string anterior el 2 y el 4 para que utilicen las variables que has creado en los pasos 1 y 2
+//Elimina la variable del paso 2 y arregla el código anterior para que todo funcione con una sola variable. Magia!
+
+var a : Int = 2
+var str : String = "\(a)+\(a)=\(a+a)"
+
+
+
